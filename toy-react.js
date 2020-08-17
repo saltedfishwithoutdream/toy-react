1ffa3e49458f9401e0258916bbff668e3566621c
linqi <857714244@qq.com>
2020-08-17 09:20:35 +0800

    第一天

---
+++ toy-react.js
@@ -0,0 +1,77 @@
+class ElementWrapper {
+    constructor (type) {
+        this.root = document.createElement(type)
+    }
+
+    setAttribute (name, value) {
+        this.root.setAttribute(name, value)
+    }
+
+    appendChild (component) {
+        this.root.appendChild(component.root)
+    }
+}
+
+class TextWrapper {
+    constructor (context) {
+        this.root = document.createTextNode(context)
+    }
+}
+
+export class Component {
+    constructor () {
+        this.props = Object.create(null)
+        this.children = []
+        this._root = null
+    }
+
+    setAttribute (name, value) {
+        this.props[name] = value
+    }
+
+    appendChild (component) {
+        this.children.push(component)
+    }
+
+    get root () {
+        if (!this._root) {
+            this._root = this.render().root;
+        }
+        return this._root
+    }
+}
+
+export function createElement (type, attrs, ...children) {
+    let e;
+    if (typeof type === 'string') {
+        e = new ElementWrapper(type)
+    } else {
+        e = new type()
+    }
+    
+    for (let p in attrs) {
+        e.setAttribute(p, attrs[p])
+    }
+
+    let insertChildren = (children) => {
+        for (let child of children) {
+            if (typeof child === 'string') {
+                child = new TextWrapper(child)
+            }
+            if (child instanceof Array) {
+                insertChildren(child)
+            } else {
+                e.appendChild(child)
+            }
+        }
+    }
+
+    insertChildren(children)
+    
+    return e
+
+}
+
+export function render (component, parentElement) {
+    parentElement.appendChild(component.root)
+}
